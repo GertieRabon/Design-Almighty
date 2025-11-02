5ba6856bdc65d0b45f6ea70857a5519db27058ff
GertieRabon <gertieannerabon@gmail.com>
2025-11-02 21:13:37 +0800

    Enhanced header functionality and styling for improved user experience. Added scroll detection to adjust header position and applied transitions. Updated main body styles and content, including padding adjustments and new about section details.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MenuService } from '../service/menu.service';
@@ -16,11 +16,22 @@ import { Menu } from '../model/menu';
 })
 export class HeaderComponent implements OnInit  {
   public menus: Menu[] = []
+  public isScrolled: boolean = false;
 
   constructor(private menuService: MenuService) {
   }
 
   ngOnInit(): void {
       this.menuService.getData().subscribe(data => {this.menus = data; });
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 10;
+    if (this.isScrolled) {
+      document.body.classList.add('header-at-top');
+    } else {
+      document.body.classList.remove('header-at-top');
+    }
   }
 }
